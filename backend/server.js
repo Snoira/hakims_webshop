9bd950da7a922ccf69d17d44da413ea2aa974547
Nora Velde <noravelde@gmail.com>
2024-04-02 09:59:49 +0200

    installed nodemon and dotenv. Added "dev" tp package.json

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { connect } = require('./config/mongoose.js');
 
 const port = process.env.PORT || 3000
 //const DATABASE_PATH = process.env.DATABASE_PATH || 'mongodb://127.0.0.1:27017/notes'
-const MONGOOSE_LIVE_URI = process.env.MONGOOSE_LIVE_URI || 'mongodb+srv://webbkvalit23:pEppzmQXNLVVKCDh@hakims-webshop.jjmlmcj.mongodb.net/?retryWrites=true&w=majority&appName=hakims-webshop';
+const MONGOOSE_LIVE_URI = process.env.MONGOOSE_LIVE_URI || 'mongodb://127.0.0.1:27017/notes'//'mongodb+srv://webbkvalit23:pEppzmQXNLVVKCDh@hakims-webshop.jjmlmcj.mongodb.net/?retryWrites=true&w=majority&appName=hakims-webshop';
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
