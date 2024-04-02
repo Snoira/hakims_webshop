5719b5e18f5917ad62e4db5c1d8e80000bf18ac6
Nora Velde <noravelde@gmail.com>
2024-04-02 16:01:33 +0200

    test igen

--- backend/app/routes/product.route.js
+++ backend/app/routes/product.route.js
@@ -3,10 +3,12 @@ const { createProduct, getProducts, startMessage } = require('../controllers/pro
 
 const productsRouter = Router()
 
-productsRouter.post('/', createProduct)
-productsRouter.get('/', getProducts)
+
 productsRouter.get('/', startMessage)
 productsRouter.get('', startMessage)
 productsRouter.get('/start', startMessage)
+productsRouter.post('/', createProduct)
+productsRouter.get('/', getProducts)
+
 
 module.exports = productsRouter
