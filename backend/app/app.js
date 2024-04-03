4efd03abfb448a5715e8bb94eaa2cca8ada7c66b
Nora Velde <noravelde@gmail.com>
2024-04-03 10:14:49 +0200

    created cors function

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const { urlencoded } = require('express');
 const productsRouter = require('./routes/product.route.js')
 
 const app = express()
+
+app.use(
+    cors({
+        origin: 'https://hakims-webshop-frontend.onrender.com',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true
+    })
+)
 
 app.use(urlencoded({ extended: true }))
 app.use(express.json())
