884f0fc02f44b3b547f4d5225660c65779dba4dd
Saina Firooz <saina.firooz@gmail.com>
2024-04-08 15:01:56 +0200

    update product function

--- backend/test/editProducts.js
+++ backend/test/editProducts.js
@@ -14,6 +14,40 @@ async function editProducts() {
             choices: products.map(p => p.name)
         },
     ]);
+
+    const product = products.find(p => p.name === productName);
+
+    const updatedProduct = await inquirer.default.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: `Enter new name for the product (current: ${product.name}):`,
+            default: product.name,
+        },
+        {
+            type: 'input',
+            name: 'category',
+            message: `Enter new category for the product (current: ${product.category}):`,
+            default: product.category,
+        }, 
+        {
+            type: 'input',
+            name: 'price',
+            message: `Enter new price for the product (current: ${product.price}):`,
+            default: product.price, 
+        }
+    ]);
+
+    Object.assign(product, updatedProduct);
+
+    fs.writeFileSync('./test/products.json', JSON.stringify(products, null, 2));
+
+    console.log(`Product has been updated: \n
+    ---------------------------\n
+    Name: ${product.name}\n
+    Category: ${product.category}\n
+    Price: ${product.price}\n 
+    ---------------------------\n`);
 }
 
 editProducts();
