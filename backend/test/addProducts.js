2d92c3bad591ef2e72a4793cbf71975fc3b768ce
Saina Firooz <saina.firooz@gmail.com>
2024-04-08 12:58:39 +0200

    try catch added and clearer console log

--- backend/test/addProducts.js
+++ backend/test/addProducts.js
@@ -1,50 +1,53 @@
 const fs = require("fs");
 
 async function addProducts() {
-  const inquirer = await import("inquirer");
+  try {
+    const inquirer = await import("inquirer");
 
-  const product = await inquirer.default.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter product name: ",
-    },
-    {
-      type: "input",
-      name: "category",
-      message: "Enter product category: ",
-    },
-    {
-      type: "input",
-      name: "price",
-      message: "Enter product price: ",
-    },
-  ]);
+    const product = await inquirer.default.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter product name: ",
+      },
+      {
+        type: "input",
+        name: "category",
+        message: "Enter product category: ",
+      },
+      {
+        type: "input",
+        name: "price",
+        message: "Enter product price: ",
+      },
+    ]);
 
-  const data = fs.readFileSync("./test/products.json");
-  const products = JSON.parse(data);
+    const data = fs.readFileSync("./test/products.json");
+    const products = JSON.parse(data);
 
-  products.push(product);
+    products.push(product);
 
-  fs.writeFileSync("./test/products.json", JSON.stringify(products, null, 2));
+    fs.writeFileSync("./test/products.json", JSON.stringify(products, null, 2));
 
-  console.log(
-    "Product has been added successfully to the products.json file.",
-    product
-  );
+    console.log(
+      `Product has been added successfully to the products.json file.\n New product:\n Name: ${product.name} \n Category: ${product.category} \n Price: ${product.price}.\n`
+    );
 
-  const { addAnother } = await inquirer.default.prompt([
-    {
-      type: "list",
-      name: "addAnother",
-      message: "Do you want to add another product?",
-      choices: ["Yes", "No"],
-    },
-  ]);
+    const { addAnother } = await inquirer.default.prompt([
+      {
+        type: "list",
+        name: "addAnother",
+        message: "Do you want to add another product?",
+        choices: ["Yes", "No"],
+      },
+    ]);
 
-  if (addAnother === "Yes") {
-    addProducts();
-  } 
+    if (addAnother === "Yes") {
+      addProducts();
+    }
+  } catch (error) {
+    console.error("Error adding product: ", error);
+  }
 }
 
 addProducts();
