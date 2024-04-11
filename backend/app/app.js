95d7b34f55aafcb1b496c6e5148f0f26c046f54a
Elin Östman <elin.ostman@hotmail.com>
2024-04-11 18:05:21 +0200

    createOrder, summering av totalprice för order, total productPrice, total price with tax, dagens datum

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const { urlencoded } = require('express');
-const productsRouter = require('./routes/product.route.js')
-const categoryRouter = require('./routes/category.route.js');
+const express = require("express");
+const cors = require("cors");
+const { urlencoded } = require("express");
+const productsRouter = require("./routes/product.route.js");
+const categoryRouter = require("./routes/category.route.js");
+const orderRouter = require("./routes/order.route.js");
 
-const app = express()
+const app = express();
 
 app.use(
-    cors({
-        origin: ['https://hakims-webshop-frontend.onrender.com', 'http://localhost:5173', 'http://127.0.0.1:5173'],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    })
-)
+  cors({
+    origin: [
+      "https://hakims-webshop-frontend.onrender.com",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(urlencoded({ extended: true }))
-app.use(express.json())
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use("/products", productsRouter);
+app.use("/categories", categoryRouter);
+app.use("/orders", orderRouter);
 
-app.use('/products', productsRouter)
-app.use('/categories', categoryRouter);
+app.get("/", (req, res) => res.send("Hello World from /!"));
 
-
-app.get('/', (req, res) => res.send('Hello World from /!'))
-
-
-module.exports = app
+module.exports = app;
