232da9bdccdbf7d49e6a1c3e48b63f4fe166389b
Elin Östman <elin.ostman@hotmail.com>
2024-04-13 16:15:02 +0200

    checkout formulär

--- frontend/src/Components/Checkout.jsx
+++ frontend/src/Components/Checkout.jsx
@@ -4,10 +4,10 @@ const CheckOut = () => {
     return (
         <> 
         <div className="py-5 text-center checkout-container">
-           <h2>checkout form</h2>
-           <p className="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+           <h2>Kassa</h2>
+           <p className="lead"> Fyll i och spara dina kunduppgifter. Därefter kan du lägga din beställning!</p>
         </div>
-        <div className="row g-5">
+        <div className="row g-5 form-container">
             <div className="col-md-5 col-lg-4 order-md-last">
                 <h4 className="d-flex justify-content-between align-items-center md-3">
                     <span className="text-primary">Varukorg</span>
@@ -39,6 +39,10 @@ const CheckOut = () => {
             <strong>200kr</strong>
           </li>
         </ul>
+        <div>
+            <button className="w-100 btn btn-primary btn-lg" type="submit">Lägg order</button>
+        </div>
+        </div>
 
         {/* formulär för kundinfo */}
         <div className="col-md-7 col-lg-8">
@@ -55,15 +59,31 @@ const CheckOut = () => {
                     </div>
                     <div className="col-12">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" placeholder="you@example.com"/>
                     </div>
+                    <div class="col-12">
+              <label for="address" className="form-label">Adress</label>
+              <input type="text" className="form-control" id="address" placeholder=""  />
+            </div>
+            <div className="col-md-3">
+              <label for="zip" className="form-label">Postnummer</label>
+              <input type="text" className="form-control" id="zip" placeholder="" required="" />
+              <div className="invalid-feedback">
+                Zip code required.
+              </div>
+            </div>
+            <div className="col-sm-9">
+              <label for="address2" className="form-label">City</label>
+              <input type="text" className="form-control" id="address2"  />
+            </div>
+            <button className="w-100 btn btn-primary btn-lg" type="submit">Spara kundinfo</button>
                 </div>
 
             </form>
 
         </div>
 
-            </div>
+           
 
         </div>
         </>
