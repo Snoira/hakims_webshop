b230c017383d946ccb2ab3038e5a7fbd61fdd3c5
Nora Velde <noravelde@gmail.com>
2024-04-17 17:09:33 +0200

    installed react toaster and i'm currently trying to fix category bug in productCardAdmin

--- backend/app/routes/category.route.js
+++ backend/app/routes/category.route.js
@@ -1,10 +1,11 @@
 const Router = require('express')
-const { createCategory, getCategories, editCategory, deleteCategory} = require('../controllers/category.controller.js')
+const { createCategory, getCategories, getCategory, editCategory, deleteCategory} = require('../controllers/category.controller.js')
 
 const categoryRouter = Router()
 
 categoryRouter.post('/', createCategory)
 categoryRouter.get('/', getCategories)
+categoryRouter.get('/:id', getCategory)
 categoryRouter.put('/edit/:id', editCategory)
 categoryRouter.delete('/delete/:id', deleteCategory)
 
